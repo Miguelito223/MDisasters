a9aa218342d3aedf9c2615519dea9333b930af6d
Miguel Jimenez <miguelitojimenez291126@gmail.com>
2025-03-18 15:30:36 +0100

    fixed

---
+++ lua/MDisasters/func/sh_netstrings.lua
@@ -0,0 +1,13 @@
+util.AddNetworkString( "md_clmenu_vars" )
+
+net.Receive( "md_clmenu_vars", function( len, pl )
+	if !pl:IsAdmin() or !pl:IsSuperAdmin() then return end
+	
+	local cvar = net.ReadString();
+	local val = net.ReadFloat();
+
+	if( GetConVar( tostring( cvar ) ) == nil ) then return end
+	if( GetConVar( tostring( cvar ) ):GetInt() == tonumber( val ) ) then return end
+
+	game.ConsoleCommand( tostring( cvar ) .." ".. tostring( val ) .."\n" );
+end)
